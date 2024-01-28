c82a1746fc9afbf4d5eb22b63048bb9351dfd6c9
ajaySankapal <ajay.sankapal@tensorgo.com>
2024-01-28 23:28:15 +0530

    redux implemented and toolbox and menu made functional

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,48 +5,34 @@ import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from
 
 import styles from './index.module.css'
 
-// import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
 
-// import { MENU_ITEMS } from '@/constants'
+import { MENU_ITEMS } from '@/constants'
 const Menu = () => {
-    // const dispatch = useDispatch()
-    // const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
-    // const handleMenuClick = (itemName) => {
-    //     dispatch(menuItemClick(itemName))
-    // }
+    const dispatch = useDispatch()
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const handleMenuClick = (itemName) => {
+        dispatch(menuItemClick(itemName))
+    }
 
-    // const handleActioItemClick = (itemName) => {
-    //     dispatch(actionItemClick(itemName))
-    // }
+    const handleActioItemClick = (itemName) => {
+        dispatch(actionItemClick(itemName))
+    }
     return (
         <div className={styles.menuContainer}>
-            <div className={cx(styles.iconWrapper,)}
-
-            //{ [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL }
-
-            // onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}
-            >
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL })} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} className={styles.icon} />
             </div>
-            <div className={cx(styles.iconWrapper,)}
-            // onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
-            >
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.ERASER })} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} className={styles.icon} />
             </div>
-            <div className={styles.iconWrapper}
-
-            // onClick={() => handleActioItemClick(MENU_ITEMS.UNDO)}
-            >
+            <div className={styles.iconWrapper} onClick={() => handleActioItemClick(MENU_ITEMS.UNDO)}>
                 <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
             </div>
-            <div className={styles.iconWrapper}
-            // onClick={() => handleActioItemClick(MENU_ITEMS.REDO)}
-            >
+            <div className={styles.iconWrapper} onClick={() => handleActioItemClick(MENU_ITEMS.REDO)}>
                 <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
             </div>
-            <div className={styles.iconWrapper}
-            // onClick={() => handleActioItemClick(MENU_ITEMS.DOWNLOAD)}
-            >
+            <div className={styles.iconWrapper} onClick={() => handleActioItemClick(MENU_ITEMS.DOWNLOAD)}>
                 <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
             </div>
         </div>
